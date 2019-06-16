dcdcff053235f2263a46ac6a693f6326d1942f4e
aleksandar727 <alexandrr@gmail.com>
2019-06-16 23:50:20 +0200

    Dodati modeli Apartman, Korisnik i enumeracija UlogaKorisnika.

---
+++ WebAplikacija/Models/Apartman.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAplikacija.Models
+{
+    public class Apartman
+    {
+    }
+}
---
+++ WebAplikacija/Models/Korisnik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAplikacija.Models
+{
+    public class Korisnik
+    {
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public UlogaKorisnika UlogaKorisnika { get; set; }
+        public List<Apartman> Apartmani { get; set; }
+        public List<Apartman> Rezervacije { get; set; }
+            
+    }
+
+    public enum UlogaKorisnika
+    {
+        Administrator,
+        Domacin,
+        Gost
+    }
+}
