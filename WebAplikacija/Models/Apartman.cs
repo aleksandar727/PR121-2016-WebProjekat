63feae3ba5211a863c780934ace883ab9e7d7b73
aleksandar727 <alexandrr@gmail.com>
2019-06-17 00:22:36 +0200

    Dodati modeli Apartman i SadrzajApartmana.

--- WebAplikacija/Models/Apartman.cs
+++ WebAplikacija/Models/Apartman.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,28 @@ namespace WebAplikacija.Models
 {
     public class Apartman
     {
+        public string TipApartmana { get; set; }
+        public int BrojSoba { get; set; }
+        public int BrojGostiju { get; set; }
+        public Lokacija Lokacija { get; set; }
+        public List<DateTime> DatumiZaIzdavanje { get; set; }
+        public List<DateTime> DostupnostPoDatumima { get; set; }
+        public Korisnik Domacin { get; set; }
+        public List<string> KomentariGostiju { get; set; }
+        public List<Image> SlikeApartmana { get; set; }
+        public double CenaPoNoci { get; set; }
+        public string VremeZaPrijavu { get; set; } = "2 PM";
+        public string VremeZaOdjavu { get; set; } = "10 AM";
+        public Status Status { get; set; }
+        public List<SadrzajApartmana> ListaSadrzajaApartmana { get; set; }
+        public List<Korisnik> RezervacijeKorisnik { get; set; }
+
+
+    }
+
+    public enum Status
+    {
+        Aktivno,
+        Neaktivno
     }
 }
