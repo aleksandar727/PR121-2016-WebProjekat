5d325fddf9d228375107a4adaa52da18e4db9da2
aleksandar727 <alexandrr@gmail.com>
2019-06-17 00:09:01 +0200

    Dodat model SadrzajApartmana.

---
+++ WebAplikacija/Models/SadrzajApartmana.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAplikacija.Models
+{
+    public class SadrzajApartmana
+    {
+        public int Id { get; set; }
+        public string NazivSadrzaja { get; set; }
+    }
+}
