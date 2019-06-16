679d1a4305c41e91e01ce503531f914af2bab2b4
aleksandar727 <alexandrr@gmail.com>
2019-06-16 23:59:36 +0200

    Dodat model Lokacija.

---
+++ WebAplikacija/Models/Lokacija.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAplikacija.Models
+{
+    public class Lokacija
+    {
+        public decimal GeografskaSirina { get; set; } // decimal zbog preciznosti
+        public decimal GeografskaDuzina { get; set; }
+        public string Adresa { get; set; }
+    }
+}
