8c40ea251bbb2946abe0b632ffc22c323a3ddb6f
aleksandar727 <alexandrr@gmail.com>
2019-06-17 00:07:43 +0200

    Dodat model Adrese. Izmena u modelu Lokacija [polje Adresa je dobila tip Adresa umesto string].

---
+++ WebAplikacija/Models/Adresa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAplikacija.Models
+{
+    public class Adresa
+    {
+        public string Ulica { get; set; }
+        public int Broj { get; set; }
+        public string NaseljenoMesto {get; set;}
+        public int PostarinskiBroj { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Ulica} {Broj}\n{NaseljenoMesto} {PostarinskiBroj}\n";
+        }
+    }
+}
--- WebAplikacija/Models/Lokacija.cs
+++ WebAplikacija/Models/Lokacija.cs
@@ -9,6 +9,11 @@ namespace WebAplikacija.Models
     {
         public decimal GeografskaSirina { get; set; } // decimal zbog preciznosti
         public decimal GeografskaDuzina { get; set; }
-        public string Adresa { get; set; }
+        public Adresa Adresa { get; set; }
+
+        public override string ToString() // Sirina pa Duzina
+        {
+            return $"{Adresa.ToString()}{GeografskaSirina}, {GeografskaDuzina}";
+        }
     }
 }
